81fa1a8f0f052f4c5fb07d9ea989e6242c5ad515
Jesse Goodnoe <jessegoodnoe@d-173-250-137-21.dhcp4.washington.edu>
2012-04-03 19:27:16 -0700

    save the old method.

--- lib/string.rb
+++ lib/string.rb
@@ -1,6 +1,14 @@
 class String
+  
+  alias_method "orig_empty?".to_sym, "empty?".to_sym
+  
   def empty?
-    warn "Monkeys!"
-    self.gsub(/\s/, '').size <= 0
+    if (true)
+      warn "Monkeys!"
+      self.gsub(/\s/, '').size <= 0
+    else
+      self.orig_empty?
+    end
   end
+  
 end
